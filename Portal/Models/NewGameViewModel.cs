21a2fad5c3b6716c2a36f86de90d157eee9ac6d7
Johan Smarius <jsmarius@hotmail.com>
2020-09-07 11:55:41 +0200

    Code in class

---
+++ Portal/Models/NewGameViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Portal.Models
+{
+    public class NewGameViewModel
+    {
+        public DateTime PlayTime { get; set; }
+
+        public DateTime? DepartureTime { get; set; }
+
+        public bool IsHomeGame { get; set; }
+
+        public int CoachId { get; set; }
+
+        public int LaundryDutyId { get; set; }
+
+        [Required]
+        public string Opponent { get; set; }
+    }
+}
