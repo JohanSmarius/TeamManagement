4c922f6f2a6ce3ce90e8718d5b479476823d6229
Johan Smarius <jsmarius@hotmail.com>
2020-09-15 22:03:29 +0200

    Added test to show how to perform validation

---
+++ Portal.Tests/NewGameViewModelTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Portal.Models;
+using Xunit;
+
+namespace Portal.Tests
+{
+    public class NewGameViewModelTests
+    {
+        [Fact]
+        public void Opponent_Required_Should_Be_Checked()
+        {
+            // Arrange
+            var validationResults = new List<ValidationResult>();
+
+            // Act
+            var sut = new NewGameViewModel();
+            var ctx = new ValidationContext(sut, null, null);
+            Validator.TryValidateObject(sut, ctx, validationResults, true);
+
+            // Assert
+            Assert.Single(validationResults);
+            Assert.Contains(validationResults, validationResult =>
+                validationResult.MemberNames.Contains("Opponent") && validationResult.ErrorMessage.Contains("required"));
+        }
+
+    }
+}
